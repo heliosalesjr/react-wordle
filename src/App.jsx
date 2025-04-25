86279dc9516ab26361db974c47b1554689ee88bc
Helio Sales Jr <heliosalesjr@gmail.com>
2025-04-25 15:22:41 -0300

    starter

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ function App() {
 
 
   return (
-    <>
-     <h1 className="py-16 text-center bg-pink-500 text-5xl">Hello, Wordle!</h1>
-    </>
+    <div className="App">
+      <h1>My Wordle</h1>
+    </div>
   )
 }
 
