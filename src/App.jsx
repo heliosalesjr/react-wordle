7123dbd21d2329119260f222443789cf589f1b77
Helio Sales Jr <heliosalesjr@gmail.com>
2025-04-25 15:41:43 -0300

    show random word from the json

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
+import { useEffect, useState } from 'react'
 
 function App() {
-
+  const [solution, setSolution] = useState(null)
+  
+  useEffect(() => {
+    fetch('http://localhost:3001/solutions')
+      .then(res => {
+        if (!res.ok) throw new Error('Erro ao buscar soluções')
+        return res.json()
+      })
+      .then(json => {
+        const randomSolution = json[Math.floor(Math.random() * json.length)]
+        setSolution(randomSolution.word)
+      })
+      .catch(err => console.error(err.message))
+  }, [])
+  
 
   return (
     <div className="App">
       <h1>My Wordle</h1>
+      {solution && <div>Solution is: {solution}</div>}
     </div>
   )
 }
