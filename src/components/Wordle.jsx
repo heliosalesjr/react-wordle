bc2e660211053b6c01e4507351c474da31772dca
Helio Sales Jr <heliosalesjr@gmail.com>
2025-04-28 23:05:58 -0300

    adding new guesses

--- src/components/Wordle.jsx
+++ src/components/Wordle.jsx
@@ -2,19 +2,22 @@ import React, { useEffect } from 'react'
 import useWordle from '../hooks/useWordle'
 
 export default function Wordle({ solution }) {
-  const { currentGuess, handleKeyup } = useWordle(solution)
-
+  const { currentGuess, guesses, turn, isCorrect, handleKeyup } = useWordle(solution)
+  
   useEffect(() => {
     window.addEventListener('keyup', handleKeyup)
 
     return () => window.removeEventListener('keyup', handleKeyup)
   }, [handleKeyup])
 
+  useEffect(() => {
+    console.log(guesses, turn, isCorrect)
+  }, [guesses, turn, isCorrect])
+
   return (
     <div>
-        <div>Solution: { solution } </div>
-        <div>Current Guess - {currentGuess}</div>
+      <div>solution - {solution}</div>
+      <div>Current Guess - {currentGuess}</div>
     </div>
   )
 }
-
